9f7c531a39a1ccb3ad223cbee8c2437d7e92b753
dstritzel <derek.stritzel@rackspace.com>
2018-07-26 10:18:08 -0400

    Fixes PEP8 tests by reducing single line size of REGIONS

--- poppy/provider/akamai/geo_zone_code_mapping.py
+++ poppy/provider/akamai/geo_zone_code_mapping.py
@@ -19,7 +19,14 @@ from poppy.model.helpers import geo_zones
 
 LOG = log.getLogger(__name__)
 
-REGIONS = ['North America', 'South America', 'EMEA', 'Japan', 'India', "Australia", 'APAC']
+REGIONS = [
+        'North America',
+        'South America',
+        'EMEA',
+        'Japan',
+        'India',
+        'Australia',
+        'APAC']
 
 REGION_COUNTRY_MAPPING = {
     'North America': [
